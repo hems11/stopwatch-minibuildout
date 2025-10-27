3429bed2b72fe2af5497c59400207cb5f9bcc92f
hemali1110 <hemalivelani11@gmail.com>
2025-10-27 19:11:27 +0530

    StopWatch fuctionality

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+
 const StopWatch = () => {
+
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    
+    useEffect(() =>{
+        let intervalID;
+        if(isRunning){
+            intervalID = setInterval(() =>{
+                setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+            }, 1000);
+        }
+        return () => clearInterval(intervalID);
+    },[isRunning]);
+
+    const handleStartStop = (e) => {
+      setIsRunning((prevIsRunning) => !prevIsRunning);
+    }
+
+    const handleReset = () => {
+        setIsRunning(false);
+        setElapsedTime(0);
+    }
+
+    const formatTime = (seconds) =>{
+        const minutes = Math.floor(seconds/60);
+        const remainingSeconds = seconds % 60 ;
+        return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+
+    }
     return (
-        <div>
+        <div style={{padding:"10px"}}>
             <h1>StopWatch</h1>
+            <p>Time: {formatTime(elapsedTime)}</p>
+            <button onClick={handleStartStop}>
+                {isRunning ? "Stop" : "Start"}
+            </button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     )
 }
